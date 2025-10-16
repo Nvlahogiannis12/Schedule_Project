c398369e0ddecdd2c6b5fc47368bdef84dff992f
Nick Vlahogiannis <526Nvlahogiannis@frhsd.com>
2025-10-16 11:53:18 -0400

    Magic Tricks

---
+++ script.js
@@ -0,0 +1,22 @@
+let jsonFile = "";
+
+window.addEventListener("keydown", (event) => {
+  if (event.key === "1") {
+    fetch("nick.json")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        data.forEach((element) => {
+          document.getElementById("out").insertAdjacentHTML(
+            "beforebegin",
+            `<h2>${element.className}</h2>
+<p>${element.teachers}</p>
+<p>${element.roomNumber}</p>
+<p>${element.period}</p>
+<p>${element.subjectArea}</p>
+`
+          );
+        });
+      });
+  }
+});
