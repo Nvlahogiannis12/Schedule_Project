9c5eb14eb7d56a8bd50243fc550081b34d276373
Nick Vlahogiannis <526Nvlahogiannis@frhsd.com>
2025-10-20 10:24:23 -0400

    cards working

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
 let jsonFile = "";
 
 window.addEventListener("keydown", (event) => {
-  if (event.key === "1") {
-    fetch("nick.json")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        data.forEach((element) => {
-          document.getElementById("out").insertAdjacentHTML(
-            "beforebegin",
-            `<h2>${element.className}</h2>
-<p>${element.teachers}</p>
-<p>${element.roomNumber}</p>
-<p>${element.period}</p>
-<p>${element.subjectArea}</p>
-`
-          );
-        });
-      });
-  } else if(event.key === "2"){
-    fetch("cooper.json")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        data.forEach((element) => {
-          document.getElementById("out").insertAdjacentHTML(
-            "beforebegin",
-            `<h2>${element.className}</h2>
-<p>${element.teachers}</p>
-<p>${element.roomNumber}</p>
-<p>${element.period}</p>
-<p>${element.subjectArea}</p>
-`
-          );
-        });
-      });
+  document.getElementById("out").textContent = "";
+  let fileName = "nick.json";
+  switch (event.key) {
+    case "1":
+      fileName = "nick.json";
+      break;
+    case "2":
+      fileName = "cooper.json";
+      break;
+    case "3":
+      fileName = "hayley.json";
+      break;
+    case "4":
+      fileName = "layla.json";
+      break;
   }
+  fetch(fileName)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      data.forEach((element) => {
+        document.getElementById("out").insertAdjacentHTML(
+          "beforeend",
+          `
+            <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h3 class="card-title">${element.className}</h3>
+    <h4 class="card-subtitle mb-2 text-body-secondary">${element.teacher}</h4>
+    <p class="card-text">${element.roomNumber}</p>
+    <p class="card-text">${element.period}</p>
+    <p class="card-text">${element.subjectArea}</p>
+  </div>
+</div>
+          `
+        );
+      });
+    });
 });
+
+//Sources
+//Remy
+//Kilgore
