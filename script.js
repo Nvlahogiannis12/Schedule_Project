15d8607e515abdd8b1d933a0c164c8c164cfe7f4
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2025-10-20 12:40:52 +0000

    cooper not working :/

--- script.js
+++ script.js
@@ -18,5 +18,22 @@ window.addEventListener("keydown", (event) => {
           );
         });
       });
+  } else if(event.key === "2"){
+    fetch("cooper.json")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        data.forEach((element) => {
+          document.getElementById("out").insertAdjacentHTML(
+            "beforebegin",
+            `<h2>${element.className}</h2>
+<p>${element.teachers}</p>
+<p>${element.roomNumber}</p>
+<p>${element.period}</p>
+<p>${element.subjectArea}</p>
+`
+          );
+        });
+      });
   }
 });
